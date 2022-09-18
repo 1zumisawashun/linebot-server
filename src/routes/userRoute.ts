d937bd33ad20843a52ccffb331db57b0d60b32ad
1zumisawashun <shun.1zumisawa@gmail.com>
2022-09-18 17:55:44 +0900

    prismaのCRUD機能の実装

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,24 +1,78 @@
 import { Router } from "express";
 const router = Router();
 
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import { PrismaClient, Prisma } from "@prisma/client";
+const prisma = new PrismaClient({ rejectOnNotFound: true });
+
+const errorMessage =
+  "There is a unique constraint violation, a new user cannot be created with this email";
 
 router.get("/users", async (req, res) => {
   const users = await prisma.user.findMany();
   return res.json(users);
 });
 
-router.post("/users", async (req, res) => {
-  const { name, email } = req.body;
-  console.log(name, email, "name, email");
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
+router.get("/users/:id", async (req, res) => {
+  const id = +req.params.id;
+  const user = await prisma.user.findUnique({
+    where: {
+      id,
     },
   });
   return res.json(user);
+});
+
+router.post("/users", async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+      },
+    });
+    return res.json(user);
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        console.log(errorMessage);
+      }
+    }
+    return res.status(400).json(error);
+  }
+});
+
+router.put("/users/:id", async (req, res) => {
+  const id = +req.params.id;
+  const { name } = req.body;
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+      },
+    });
+    return res.json(user);
+  } catch (e) {
+    return res.status(400).json(e);
+  }
+});
+
+router.delete("/users/:id", async (req, res) => {
+  const id = +req.params.id;
+
+  try {
+    const user = await prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+    return res.json(user);
+  } catch (e) {
+    return res.status(400).json(e);
+  }
 });
 
 export default router;
